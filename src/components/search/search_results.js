3967025d95cb7b8a1657d518f86087ee0fe3f6ed
jbianchi <joseph.bianchi@swiftmedical.io>
2019-10-17 12:52:05 -0400

    added car cars in renderList

--- src/components/search/search_results.js
+++ src/components/search/search_results.js
@@ -2,8 +2,28 @@ import React from 'react';
 import { connect } from 'react-redux';
 import _ from 'lodash';
 import { FaCarAlt, FaMoneyCheckAlt } from "react-icons/fa";
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Card from 'react-bootstrap/Card'
 
 class SearchResults extends React.Component {
+
+  renderList = () => {
+    return this.props.cars.map(car => {
+      return(
+        <Col xs={12} sm={6} md={4}>
+          <Card border="primary" style={{ width: '18rem' }}>
+            <Card.Header>${car.price}</Card.Header>
+            <Card.Body>
+              <Card.Title>{car.heading}</Card.Title>
+              <Card.Img variant="bottom" src={car.media.photo_links[0]} />
+            </Card.Body>
+          </Card>
+        </Col>
+      )
+    })
+  }
 
 
   render() {
@@ -29,6 +49,11 @@ class SearchResults extends React.Component {
           />
           </span>
           <h2>Average Price: {avgPrice}</h2>
+          <Container fluid={true}>
+            <Row>
+              {this.renderList()}
+            </Row>
+          </Container>
           <div>
           </div>
       </div>
